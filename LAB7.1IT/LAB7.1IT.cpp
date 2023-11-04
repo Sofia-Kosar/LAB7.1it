a739f2f319bed2399a71818d8f875a679e193c94
Sofia-kosar <Sofiia.Kosar.RI.2023@lpnu.ua>
2023-11-05 00:38:31 +0200

    first commit

--- LAB7.1IT/LAB7.1IT.cpp
+++ LAB7.1IT/LAB7.1IT.cpp
@@ -9,7 +9,7 @@ void Create(int** a, const int rowCount, const int colCount, const int Low,
     const int High);
 void Print(int** a, const int rowCount, const int colCount);
 void Sort(int** a, const int rowCount, const int colCount);
-void Change(int** a, const int row1, const int row2, const int colCount);
+void Change(int** a, const int col1, const int col2, const int rowCount);
 int Calc(int** a, const int rowCount, const int colCount, int& S, int& k);
 int main()
 {
